1d2f4eeeb27f1a6951775549bab75745b2b13895
niole <niolenelson@gmail.com>
2019-07-27 13:02:04 -0700

    Properly name test

--- src/__tests__/withDataGetter.test.tsx
+++ src/__tests__/withDataGetter.test.tsx
@@ -5,7 +5,18 @@ import withDataGetter from '../withDataGetter';
 describe('withDataGetter', () => {
     const ChildComponent = (props:  { text: string }) => props.text;
 
-    it('should allow child to render', done => {
+    it('should allow child to render', () => {
+        const WrappedComponent = withDataGetter<{}, { text: string }>(
+            async () => ({ text: 'fetched' }),
+            () => ({ text: 'nothing' })
+        )(ChildComponent);
+
+        const wrapper = mount(<WrappedComponent />);
+
+        expect(wrapper.find(ChildComponent)).toHaveLength(1);
+    });
+
+    it('should update child state on component did mount', done => {
         const WrappedComponent = withDataGetter<{}, { text: string }>(
             async () => ({ text: 'fetched' }),
             () => ({ text: 'nothing' })
@@ -21,4 +32,5 @@ describe('withDataGetter', () => {
             done();
         }, 10);
     });
+
 });
